2537564e8b6ec5e24d91f44d9336001a554a228d
Jorge Sanchez <jorge@nellisauction.com>
2024-09-04 14:26:08 -0700

    Saving ch2L8

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pygame
+from player import Player
 
 # Constants for screen dimensions
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-
+# Player constants
+PLAYER_RADIUS = 20
 def main():
     pygame.init()  # Initialize Pygame
+    player = Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2, radius=PLAYER_RADIUS) # Initialize the player object
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Set up the display
-
+    clock= pygame.time.Clock()
+    dt=0
     running = True  # Variable to control the main loop
 
     while running:
@@ -18,12 +22,13 @@ def main():
         # Update game state here
 
         screen.fill((0, 0, 0))  # Fill the screen with black color
-
+        player.update(dt)
         # Draw everything here
-
+        player.draw(screen)
         pygame.display.flip()  # Update the display
+        dt = clock.tick(60) / 1000
 
     pygame.quit()  # Clean up and exit Pygame
-
+    
 if __name__ == "__main__":
     main()
